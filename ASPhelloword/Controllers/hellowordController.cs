e623c86c6999fcd7ea0510743f678854f41a32fa
Ritza Kufita Rizqia <ritzakufita@gmail.com>
2017-01-06 15:51:38 +0700

    Add project files.

---
+++ ASPhelloword/Controllers/hellowordController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASPhelloword.Controllers
+{
+    public class hellowordController : Controller
+    {
+        //
+        // GET: /helloword/
+
+        public ActionResult helloword()
+        {
+            return View();
+        }
+
+    }
+}
